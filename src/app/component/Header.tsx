8b2fef46e6af650310681ee3ab27489581f08d06
Le Huu Tri <lehuutri1609@gmail.com>
2025-03-04 18:46:16 +0700

    component footer, header, modify login

---
+++ src/app/component/Header.tsx
@@ -0,0 +1,168 @@
+// src/app/components/Header.tsx
+"use client";
+import styles from "../page.module.css";
+import Link from 'next/link';
+import { useState, useEffect } from "react";
+import clsx from "clsx";
+
+export function BackHeader() {
+  return(
+    <div className={styles.backHeader}>
+      <a href="#">
+        <i className="fa-solid fa-arrow-up"></i>
+      </a>
+    </div>
+  );
+}
+
+export function Slogan() {
+  const texts = [
+    "Stationery lựa chọn số 1 cho bạn",
+    "Stationery lựa chọn số 1 cho bạn - Hãy đến với chúng tôi",
+  ];
+  const [index, setIndex] = useState(0);
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false);
+      setTimeout(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % texts.length);
+        setFade(true);
+      }, 500);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={styles.slogan}>
+      <span className={fade ? styles.fade_in : styles.fade_out}>{texts[index]}</span>
+    </div>
+  )
+}
+
+export function HeaderTop() {
+  return(
+    <div className={styles.header_top}>
+      <div className={styles.container}>
+        <div className={styles.block_top}>
+          <div className={styles.logo}>
+            <a href="#" className={styles.logo_link}>
+              <picture>
+                <img className={styles.img_logo} src="/img/logo.webp" alt=""/>
+              </picture>
+            </a>
+          </div>
+
+          <div className={styles.search}>
+            <form className={styles.header_search} role="search">
+              <input className={styles.input_search}  type="text" placeholder="Tìm kiếm sản phẩm..." autoComplete="off"/>
+            </form>
+            <div className={styles.search_icon}>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+          </div>
+          
+          <div className={styles.hotline_account}>
+            <ul className={styles.group_acc}>
+              <li className={styles.hotline}>
+                <a href="tel:19008386" title="call 19008386">
+                  <i className="fa-solid fa-square-phone"></i>
+                  <span>
+                    <strong> Hotline: </strong>
+                    1900 8386 
+                  </span>
+                </a>
+              </li>
+
+              <li>
+                <div className={styles.icon}>
+                  <a href="" title="Sản phẩm yêu thích">
+                    <i className="fa-regular fa-heart"></i>
+                    <span>0</span>
+                  </a>
+                </div>
+              </li>
+
+              <li>
+                <div className={styles.icon}>
+                  <a href="" title="Giỏ hàng">
+                    <i className="fa-solid fa-bag-shopping"></i>
+                      <span>0</span>
+                  </a>
+                </div>
+              </li>
+    
+              <li className={styles.account}>
+                <div className={styles.icon}>
+                <Link href="/login">
+                  <i className="fa-regular fa-user"></i>
+                </Link>
+              </div>
+            <div className={styles.sign_log}>
+              <Link href="/login">Đăng nhập</Link>
+              <a href="">Đăng ký</a>
+              </div>
+            </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function HeaderMenu() {
+  return(
+    <div className={styles.header_menu}>
+      <div className={styles.container}>
+        <div className={styles.block_menu}>
+
+          <div className={styles.category}>
+            <div className={styles.title}>
+              <i className="fa-solid fa-list-ul"></i>
+              Danh mục sản phẩm
+            </div>
+            <div className={styles.list_menu}>
+              <div className={styles.contentfill}>
+                <div className={styles.swiper_wrapper}></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+
+          <Slogan/>
+
+          <div className={styles.menu}>
+            <a href="" title="Khuyến mãi">
+              <i className="fa-solid fa-fire"></i>
+              Khuyến mãi
+            </a>
+            <a href="#service" title="Dịch vụ">
+              <i className="fa-solid fa-shield-heart"></i>
+              Dịch vụ
+            </a>
+            <a href="" title="Tin tức">
+              <i className="fa-solid fa-bullhorn"></i>
+              Tin tức
+            </a>
+            <a href="" title="Liên hệ">
+              <i className="fa-solid fa-square-phone"></i>
+              Liên hệ
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default function Header() {
+  return(
+    <div className={styles.header}>
+      <HeaderTop/>
+      <HeaderMenu/>
+    </div>
+  )
+}
